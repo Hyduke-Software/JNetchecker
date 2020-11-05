705d07faa05479012d3a1d26297fde7462a83d39
Hyduke <gallimore.tj@gmail.com>
2020-11-05 18:25:13 +0000

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace WpfTutorialSamples.DataGrid_control
+{
+    public static class Config
+    {
+        public static string rootFolder = @"C:\Temp\commandcentre\";
+        //Default file. MAKE SURE TO CHANGE THIS LOCATION AND FILE PATH TO YOUR FILE   
+        static public string textFile = @"C:\Temp\commandcentre\commandcentre.txt";
+        public static Dictionary<string, bool> configValuesType = new Dictionary<string, bool>(); //janky method of marking the expected value as int or char. TRUE = INT, FALSE =CHAR
+        public static Dictionary<string, string> configValues = new Dictionary<string, string>();
+        //  List<host> hosts = new List<host>();
+
+        public static void testConfigModule(List<host> hosts)
+        {
+            //default vaulues
+            hosts[1].timeseen = 69;
+        }
+
+            public static List<host> getHostNames(List<host> hosts)
+        {
+          
+            if (File.Exists(textFile))
+            {
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(textFile);
+
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    //hosts[i].hostname = lines[i]; //makes hosts[] value = lines[] value
+                    hosts.Add(new host() { hostname = lines[i]});
+                    //parts.Add(new Part() {PartName="crank arm", PartId=1234});
+
+                }
+                return hosts;
+
+            }
+            //todo: return some sort of failure
+            return hosts;
+
+            //   ValidateValues();
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Net.NetworkInformation;
+namespace WpfTutorialSamples.DataGrid_control //todo refactor into my own name
+{
+    public partial class JNetchecker : Window
+    {
+        List<host> hosts = new List<host>();
+        public JNetchecker()
+        {
+            InitializeComponent();
+
+
+            /*   hosts.Add(new host() { Id = 1, hostname = "raspberrypi", lastLiveTime = new DateTime(2020, 1, 1), MAC="" ,lastIP = "", timeseen=0});
+               hosts.Add(new host() { Id = 2, hostname = "DiceCam", lastLiveTime = new DateTime(2020, 1, 1), MAC = "", lastIP = "", timeseen = 0 });
+               hosts.Add(new host() { Id = 3, hostname = "Emerald-III", lastLiveTime = new DateTime(2020, 1, 1), MAC = "", lastIP = "", timeseen = 0 });
+               hosts.Add(new host() { Id = 4, hostname = "hyduke-webserver", lastLiveTime = new DateTime(2020, 1, 1), MAC = "", lastIP = "", timeseen = 0 });
+               hosts.Add(new host() { Id = 5, hostname = "zimbabweServer", lastLiveTime = new DateTime(2020, 1, 1), MAC = "", lastIP = "", timeseen = 0 });
+               */
+            Config.getHostNames(hosts); //gets the values from the text file
+
+
+            refreshTable(); //sets the datagrid values from the users list.
+        }
+
+
+        public void refreshTable()
+        {
+
+            dgSimple.ItemsSource = null;
+            dgSimple.ItemsSource = hosts;
+        }
+        public void Button_Click(object sender, EventArgs e)
+        {
+            //todo for length
+            pinger(0);
+            pinger(1);
+            pinger(2);
+      
+
+        }
+
+        public void pinger(int id) { 
+        Ping p = new Ping();
+        PingReply r;
+        string s;
+        s = hosts[id].hostname;
+           
+            try
+            {
+                r = p.Send(s);
+                _ = r.Status == IPStatus.Success;
+
+                textbox.Text = "Ping to " + s.ToString() + "[" + r.Address.ToString() + "]" + " Successful"
+    + " Response delay = " + r.RoundtripTime.ToString() + " ms" + "\n";
+                hosts[id].lastLiveTime = DateTime.Now;
+                hosts[id].lastIP = r.Address.ToString();
+                hosts[id].timeseen = hosts[id].timeseen + 1;
+                hosts[id].online = true;
+
+                refreshTable();
+    }
+
+            catch (Exception j)
+            {
+                hosts[id].MAC = "OFFLINE";
+                textbox.Text = "" + j;
+                hosts[id].online = false;
+
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Config.testConfigModule(hosts);
+            refreshTable();
+        }
+    }
+}
+
+public class host
+    {
+        public int Id { get; set; }
+        public string hostname { get; set; }
+        public DateTime lastLiveTime { get; set; }
+        public string MAC { get; set; }
+        public string lastIP { get; set; }
+        public int timeseen { get; set; }
+        public bool online { get; set; }
+}
+
+
+
+
